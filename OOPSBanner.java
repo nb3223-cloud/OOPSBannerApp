5848356278b3d7461fcf985fdd70c1373a7bdfa8
nb3223-cloud <nb3223@srmist.edu.in>
2026-03-11 13:03:20 +0530

    Added UC6 Static Functions

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,30 +1,72 @@
-/**
- * UC4 - Print OOPS Banner using String Array and Loop
- * Author: sysadmin
- * Version: 4.0
- */
-
 public class OOPSBanner {
 
     public static void main(String[] args) {
 
-        // Store banner lines in String array using String.join()
-        String[] banner = {
-
-            String.join(" ", "*****", "*****", "*****", "*****"),
-            String.join(" ", "*   *", "*   *", "*   *", "*   *"),
-            String.join(" ", "*   *", "*   *", "*   *", "*    "),
-            String.join(" ", "*   *", "*****", "*****", "*****"),
-            String.join(" ", "*   *", "*    ", "*    ", "    *"),
-            String.join(" ", "*   *", "*    ", "*    ", "*   *"),
-            String.join(" ", "*****", "*    ", "*****", "*****")
-
+        String[] banner = new String[] {
+            buildLine(0),
+            buildLine(1),
+            buildLine(2),
+            buildLine(3),
+            buildLine(4),
+            buildLine(5),
+            buildLine(6)
         };
 
-        // Print banner using loop
         for (String line : banner) {
             System.out.println(line);
         }
+    }
 
+    static String buildLine(int row) {
+        return String.join(" ",
+                getO(row),
+                getO(row),
+                getP(row),
+                getS(row));
+    }
+
+    static String getO(int row) {
+
+        String[] O = {
+                "*****",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*****"
+        };
+
+        return O[row];
+    }
+
+    static String getP(int row) {
+
+        String[] P = {
+                "*****",
+                "*   *",
+                "*   *",
+                "*****",
+                "*    ",
+                "*    ",
+                "*    "
+        };
+
+        return P[row];
+    }
+
+    static String getS(int row) {
+
+        String[] S = {
+                "*****",
+                "*    ",
+                "*    ",
+                "*****",
+                "    *",
+                "    *",
+                "*****"
+        };
+
+        return S[row];
     }
 }
